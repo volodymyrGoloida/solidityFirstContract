77248b047da06fa504c1a24932de3fb95634e181
Володимир Голойда <volodimirgoloida@MacBook-Air-Volodimir.local>
2023-01-14 17:37:05 +0100

    fix:update to new version of soliditry

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
-// deploy code will go here
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require("./compile");
+
+provider = new HDWalletProvider(
   process.env.PNEUMONIC,
   process.env.PROVIDER_URL
 );
@@ -12,18 +12,13 @@ const web3 = new Web3(provider);
 deploy();
 async function deploy() {
   const accounts = await web3.eth.getAccounts();
-  console.log(accounts);
+
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const { options } = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ["Hi there!"],
-    })
-    .send({
-      gas: "1000000",
-      from: accounts[0],
-    });
-  console.log("Contract deploy to ", options.address);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 }
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,41 +1,35 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
-const { interface, bytecode } = require("../compile");
-
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const INITIAL_STRING = "Hi there";
+ 
+const { abi, evm } = require('../compile');
+ 
 let accounts;
 let inbox;
+ 
 beforeEach(async () => {
-  //Get a list of all accounts
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  //use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
-      arguments: [INITIAL_STRING],
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
     })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: accounts[0], gas: '1000000' });
 });
-
-describe("Inbox", () => {
-  it("deploys a contact", () => {
+ 
+describe('Inbox', () => {
+  it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
-
-  it("has a default message", async () => {
+  it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, INITIAL_STRING);
+    assert.equal(message, 'Hi there!');
   });
-
-  it("can change a message", async () => {
-    const NEW_MESSAGE = "bye";
-    await inbox.methods.setMessage(NEW_MESSAGE).send({
-      from: accounts[0],
-    });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
-    assert.equal(message, NEW_MESSAGE);
+    assert.equal(message, 'bye');
   });
 });
